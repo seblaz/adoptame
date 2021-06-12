461fe1e9234d3f9fd623f210b8bd8c3de44d36ec
Tomas Rocchi <tomasrocchi@Tomass-MacBook-Pro.local>
2021-06-12 20:45:09 -0300

    Init repo

---
+++ packages/front/src/app/components/Routes/index.js
@@ -0,0 +1,26 @@
+import React, { Suspense } from 'react';
+import { ConnectedRouter } from 'connected-react-router';
+import { Route } from 'react-router-dom';
+
+import Home from '~screens/Home';
+import Login from '~screens/Login';
+import Playground from '~screens/Playground';
+import { history } from '~redux/store';
+import { ROUTES } from '~constants/routes';
+
+import styles from './styles.module.scss';
+import AuthenticatedRoute from './components/AuthenticatedRoute';
+
+const AppRoutesContainer = () => (
+  <ConnectedRouter history={history}>
+    <div className={`column center ${styles.container} ${styles.containerAlgo}`}>
+      <Suspense>
+        <AuthenticatedRoute path={ROUTES.LOGIN} component={Login} public />
+        <Route path={ROUTES.HOME} component={Home} exact />
+        {process.env.NODE_ENV === 'development' && <Route path={ROUTES.PLAYGROUND} component={Playground} />}
+      </Suspense>
+    </div>
+  </ConnectedRouter>
+);
+
+export default AppRoutesContainer;
