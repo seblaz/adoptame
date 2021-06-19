5064d9921122c73971b2a26c057f56e1dca57997
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2021-06-19 20:14:37 -0300

    Se agrega boton de enviar y llamada al endpoint

--- packages/front/src/app/screens/Registration/index.js
+++ packages/front/src/app/screens/Registration/index.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import Input from '~app/components/Input';
+import Button from '~app/components/Button';
+import actionCreators from '~redux/Auth/actions';
 
 import styles from './styles.module.scss';
 
 const Registration = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    dispatch(actionCreators.register({ email, password }));
+  };
 
   return (
     <div className={`column center middle ${styles.container}`}>
@@ -25,6 +33,7 @@ const Registration = () => {
         value={password}
         onChange={event => setPassword(event.target.value)}
       />
+      <Button label="Enviar" onClick={handleSubmit} />
     </div>
   );
 };
