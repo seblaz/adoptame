03be552d80138c240aeea0587c03190ebad08bcf
tomiir <rocchitomas@gmail.com>
2021-07-12 19:18:03 -0300

    Fixed edad and animal view width

--- packages/front/src/app/components/UserProfile/constants.js
+++ packages/front/src/app/components/UserProfile/constants.js
@@ -2,6 +2,6 @@ export const INFO_FIELDS = [
   { key: 'nombre', label: ' ', variant: 'h5' },
   { key: 'email', label: 'Email: ', variant: 'body1' },
   { key: 'telefono', label: 'Telefono: ', variant: 'body1' },
-  { key: 'anioDeNacimiento', label: 'Año de nacimiento: ', variant: 'body1' },
+  { key: 'anioDeNacimiento', label: 'Edad: ', variant: 'body1' },
   { key: 'zona', label: 'Zona: ', variant: 'body1' }
 ];
